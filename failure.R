0a362e83d2e8f08372814716a51acbae4c42f504
Darwin <darwin@samaritan.city>
2026-01-06 15:54:37 -0800

    initial repro

---
+++ failure.R
@@ -0,0 +1,6 @@
+# This script fails with dagu
+# it is the same as the other script, except it loads the dplyr package
+
+library(dplyr)
+
+print(1:10)
---
+++ success.R
@@ -0,0 +1,4 @@
+# This script prints 1-10.
+# It succeeds running with dagu
+
+print(1:10)
