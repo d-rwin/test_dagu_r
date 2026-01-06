1ac58ce7b9ffe7380f9e245f93ea51a2301b9a66
Darwin <darwin@samaritan.city>
2026-01-06 15:58:26 -0800

    .

---
+++ setup.R
@@ -0,0 +1,4 @@
+# installs packages to be used in examples
+
+install.packages("dplyr")
+install.packages("yaml")
--- success.R
+++ success.R
@@ -1,4 +1,5 @@
 # This script prints 1-10.
 # It succeeds running with dagu
+library(yaml)
 
 print(1:10)
